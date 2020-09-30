f00c36bd174efbdfc6818c2740d93e17e4d64f9c
Nicolas <nicoladufresne@gmail.com>
2020-09-30 11:33:12 +0200

    add mentions-legales page

---
+++ wp-content/themes/lesvikings/template-mentions-legales.php
@@ -0,0 +1,36 @@
+<?php /* Template Name: Mentions légales */?>
+
+<?php get_header(); ?>
+
+    <div class="block"></div>
+    <section id="metions-legales">
+        <div class="wrap">
+            <h1>Mentions légales</h1>
+
+            <h2>Editeur</h2>
+            <p>Directeur de publication : Emile CANU</p>
+            <address>Adresse : 16 place de l'Hôtel de ville - BP 219 - 76196 Yvetot CEDEX</address>
+
+            <h2>Conception et développement</h2>
+            <p>Service communication Stéphane Miller et Jordan Huré.</p>
+            <p>Nicolas Dufresne, NFactory Shcool.</p>
+
+            <h2>Prestataire d’hébergement</h2>
+            <p>Infosat Télécom</p>
+            <address>59 rue Caroline Herschel, Technopole du madrillet, 76800 Saint-Étienne-du-Rouvray</address>
+
+            <h2>Crédits photographiques</h2>
+            <p>Service communication et producteurs.</p>
+
+            <h2>Traitement des données personnelles :</h2>
+            <p>La loi sur l’informatique et les libertés :</p>
+            <p>Les informations qui vous concernent sont destinées à CCYN – Communauté de Communes Yvetot Normandie. Nous nous engageons à respecter les dispositions de la loi sur l’informatique et les libertés (article 27 de la loi n°78-17 du 6 janvier 1978). En conséquence, vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent.</p>
+            <p>Pour plus d’information, vous pouvez vous rendre sur le site de la CNIL.</p>
+
+            <h2>Responsabilité des partenaires</h2>
+            <p>La ville d'Yvetot n’est pas responsable des informations transmises par les partenaires, figurant sur son site internet.</p>
+        </div>
+    </section>
+    <div class="block"></div>
+
+<?php get_footer();
